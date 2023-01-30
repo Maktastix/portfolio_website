839b204e029132f295297147e015f6b8d2d2d85b
MichaelMakinde <michael_makinde@outlook.com>
2023-01-29 21:01:18 -0500

    Added scroll to top JS functionality

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const website = {};
 
+website.scrollButton = document.getElementById('back_to');
 
 
+website.scrollButton.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
+
+website.scrollFunction = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        website.scrollButton.style.display = "flex";
+    } else {
+        website.scrollButton.style.display = "none";
+    }
+}
+
+window.onscroll = function() {
+    website.scrollFunction();
+}
 
 website.init = () => {
-
+    website.scrollFunction();
 }
 
 website.init();
+
+console.log(website);
